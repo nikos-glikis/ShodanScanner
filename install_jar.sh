a585bbd485e10eed07cd9b4e6b560c50136b71dc
nikos <nikosglikis@gmail.com>
2016-05-28 17:26:31 +0300

    Using TorRange 1.0.4

--- install_jar.sh
+++ install_jar.sh
@@ -1,2 +1,2 @@
-mvn install:install-file  -DgroupId=com.object0r.scanners -DartifactId=ShodanScanner -Dversion=1.0.1 -Dpackaging=jar -Dfile=target/ShodanScanner-1.0.1-jar-with-dependencies.jar
-
+#mvn install:install-file  -DgroupId=com.object0r.scanners -DartifactId=ShodanScanner -Dversion=1.0.1 -Dpackaging=jar -Dfile=target/ShodanScanner-1.0.1-jar-with-dependencies.jar
+mvn install -DperformRelease=true -DcreateChecksum=true
